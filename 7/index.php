095ded6fb6c88f1689557b5604233b05705d60e9
vladislawmaltsev <dried.dill@gmail.com>
2019-04-13 12:45:58 +0300

    task 7

---
+++ 7/index.php
@@ -0,0 +1,14 @@
+<?php
+include "index.html";
+include "other.php";
+if (!isset($_REQUEST['url'])|| !isset($_REQUEST['type'])) return;
+$url = $_REQUEST['url'];
+$type = $_REQUEST['type'];
+if (filter_var($url, FILTER_VALIDATE_IP)) $ip = $url;
+else $ip = getIP($url);
+if ($ip)
+    switch ($type):
+        case "p": ping($ip); break;
+        case "t": tracert($ip); break;
+    endswitch;
+else echo "ip адрес не существует";
---
+++ 7/other.php
@@ -0,0 +1,34 @@
+<?php
+function ping($ip){
+    exec( "ping ".$ip, $output, $retVar);
+    if($retVar == 0):
+        $percent = 100-(int)preg_match( '/[0-9.]+%/m',array_pop($output));
+        echo "<b>$ip</b><br>Процент успешных запросов: $percent %";
+        return true;
+    else :
+        echo "The IP address, $ip, is dead";
+        return false;
+    endif;
+}
+function tracert($ip){
+    exec( "tracert ".$ip, $output, $retVar);
+    if($retVar == 0) {
+        echo "Список адресов на пути: <br> ";
+        foreach ($output as $item) {
+            echo $item."<br>";
+//            $item = explode(" ", trim($item));
+//            if (isset($item[2]) && $item[2] != "*")
+//                echo $item[2] . "<br>";
+        }
+        return true;
+    }else {
+        echo "The IP address, $ip, is dead";
+        return false;
+    }
+}
+function getIP($url){
+    if(strpos($url, 'http') !== FALSE)  $url = parse_url($url)['host'];
+    $ip = gethostbyname($url);
+    if($ip == $url) $ip = FALSE;
+    return $ip;
+}
